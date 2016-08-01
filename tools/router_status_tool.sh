88ea549d7500f4d95a70a177b07a0872814128de
Nick Zhang <thesadboy@qq.com>
2016-08-01 19:31:54 +0800

    update

--- tools/router_status_tool.sh
+++ tools/router_status_tool.sh
@@ -6,32 +6,32 @@ file=router_status.asp
 wwwfile=/www/device-map/router_status.aps
 script=$root_path/jffs/scripts/init-start
 script_text="mount --bind "$path$file" "$wwwfile
-echo '检测文件['$file']是否已经存在'
+echo 'check file['$file'] existed'
 if [ -f $path$file ];then
-  echo '文件['$file']存在,删除'
+  echo 'file ['$file'] existed, deltet'
   rm -rf $path$file
 else
-  echo '检测目录['$path']是否存在'
+  echo 'check dir ['$path'] existed'
   if [ ! -d $path ];then
-    echo '目录['$path']不存在,创建目录'
+    echo 'dir ['$path'] not existed, create'
     mkdir -p $path
   fi
 fi
-echo '从服务器下载文件'
+echo 'download file from server'
 wget -P $path $remote_file --no-check-certificate
-echo '判断[init-start]文件是否存在'
+echo 'check file [init-start] existed'
 if [ -f $script ];then
-  echo '[init-start]文件存在,判断是否有挂载指令'
+  echo 'file [init-start] existed, check cmd existed'
   grep "$script_text" $script > /dev/null
   if [ ! $? -eq 0 ];then
-    echo '挂载指令不存在,将指令增加到文件中'
+    echo 'cmd not found, append to the file'
     echo $script_text >> $script
   fi
 else
-  echo '[init-start]文件不存在,创建并将挂载指令添加进去,添加执行权限'
+  echo 'file [init-start] not existed, create it and append cmd to it, add the rights to run'
   echo '#!/bin/sh' >> $script
   echo $script_text >> $script
   chmod 775 $script
 fi
-echo '执行完成,路由重启中。。。'
+echo 'finished, reboot, enjoy...'
 reboot
