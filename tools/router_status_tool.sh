40b364a812be2d7838f10cfec13a719392582fa8
Nick Zhang <thesadboy@qq.com>
2016-08-01 19:40:00 +0800

    update

--- tools/router_status_tool.sh
+++ tools/router_status_tool.sh
@@ -6,32 +6,32 @@ file=router_status.asp
 wwwfile=/www/device-map/router_status.aps
 script=$root_path/jffs/scripts/init-start
 script_text="mount --bind "$path$file" "$wwwfile
-echo 'check file['$file'] existed'
+echo '[NOTICE] check file['$file'] existed'
 if [ -f $path$file ];then
-  echo 'file ['$file'] existed, deltet'
+  echo '[NOTICE] file ['$file'] existed, deltet'
   rm -rf $path$file
 else
-  echo 'check dir ['$path'] existed'
+  echo '[NOTICE] check dir ['$path'] existed'
   if [ ! -d $path ];then
-    echo 'dir ['$path'] not existed, create'
+    echo '[NOTICE] dir ['$path'] not existed, create'
     mkdir -p $path
   fi
 fi
-echo 'download file from server'
+echo '[NOTICE] download file from server'
 wget -P $path $remote_file --no-check-certificate
-echo 'check file [init-start] existed'
+echo '[NOTICE] check file [init-start] existed'
 if [ -f $script ];then
-  echo 'file [init-start] existed, check cmd existed'
+  echo '[NOTICE] file [init-start] existed, check cmd existed'
   grep "$script_text" $script > /dev/null
   if [ ! $? -eq 0 ];then
-    echo 'cmd not found, append to the file'
+    echo '[NOTICE] cmd not found, append to the file'
     echo $script_text >> $script
   fi
 else
-  echo 'file [init-start] not existed, create it and append cmd to it, add the rights to run'
+  echo '[NOTICE] file [init-start] not existed, create it and append cmd to it, add the rights to run'
   echo '#!/bin/sh' >> $script
   echo $script_text >> $script
   chmod 775 $script
 fi
-echo 'finished, reboot, enjoy...'
+echo '[NOTICE] finished, reboot, enjoy...'
 reboot
